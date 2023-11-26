6b006108c15efd5bd43b51f1fc16c6c15285d965
José Victor <josevictorvasc@gmail.com>
2023-11-26 10:56:07 -0300

    Arduino completed

---
+++ arduino/src/info.h
@@ -0,0 +1,14 @@
+#ifndef INFO_H
+#define INFO_H
+
+#define DHT_PIN_DATA	5
+#define LEDB_PIN_VIN	4
+#define LEDR_PIN_VIN	14
+const char* SSID = "Tartarus";
+const char* PASSWORD = "nc23nb56";
+const char* SETTINGS_PATH = "http://192.168.31.92:8000/settings";
+const char* LOGGER_PATH = "http://192.168.31.92:8000/logs";
+#define SETTINGS_DELAY 3000
+#define LOGGER_DELAY 60000
+
+#endif
